acf67db690fa379898bcfc15b0ad997665ade403
spatterspace <me@spatter.space>
2024-03-29 15:04:42 -0400

    make division an object instead of an array

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@ import type { Division } from './components/tabs/types';
 // const div1: Division[] = [[4,2], [4,4]]
 // const div2: Division[] = [[2,1], [2,1], [4,4]]
 // const div3: Division[] = [[1,0.5], [1, 0.5], [2,1], [4,4]]
-const d = (...nums: number[]) => [...nums.map(n => [n, []] as Division)];
+const d = (...nums: number[]) => [...nums.map(n => ({length: n, notes: []}))];
 const divnum = ref(d(1/2,1/6,1/6,1/6,1/2,1/4,1/4,2));
 const another = ref(d(1,1,1,1));
 
@@ -13,7 +13,7 @@ const another = ref(d(1,1,1,1));
 </script>
 <template>
   <!-- <input type="checkbox" v-model="showDivisions"/> -->
-  <input type="number"/>
+  <input type="number" min="2" max="4"/>
   <TabsTabBar v-model="divnum"></TabsTabBar>
   <TabsTabBar :model-value="another"</TabsTabBar>
   <Fretboard width="50%"/>
