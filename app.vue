af9f540f941ede8a459e06daad9b7d97ef52ec7f
Dan <vocalunrest@icloud.com>
2024-04-02 17:33:12 -0400

    update plan

--- app.vue
+++ app.vue
@@ -4,6 +4,15 @@ import { createEmptyDivision } from './components/tabs/data';
 
 
 const divnum = ref([0.5, 0.5, 2, 1].map(d => createEmptyDivision(d, 6)));
+// Create a grid with X divisions, where X = total / smallest
+// Each division occupies Y columns, where Y = duration / smallest
+// The minimum size of any column is min-width
+// This setup keeps all divisions proportional to each other at all times
+// If you break a division and the resulting divisions are smaller than smallest,
+// the division you're breaking becomes its own TabBar, with expandAll turned on. You can toggle it
+// ^ but this doesn't make sense... if I have [1,1,1,1] and they all have plenty of width, I don't need a toggleable division
+// but how would we know if a division is too small? we'd need to know the width of the division is 
+// we could try container queries...
 
 divnum.value[0].notes[1].note = 70;
 
