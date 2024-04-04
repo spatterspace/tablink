7e37113dda245439f3aca8e51a6e99f961f7b7fd
Dan <vocalunrest@icloud.com>
2024-04-04 16:56:51 -0400

    rendering first pass

--- app.vue
+++ app.vue
@@ -1,40 +1,20 @@
 <script setup lang="ts">
-import type { Division } from './components/tabs/data';
-import { createEmptyDivision } from './components/tabs/data';
+import { createDivision, type Division } from './components/tabs/data';
 
 
-const divnum = ref([0.5, 0.5, 2, 1].map(d => createEmptyDivision(d, 6)));
-// Create a grid with X divisions, where X = total / smallest
-// Each division occupies Y columns, where Y = duration / smallest
-// The minimum size of any column is min-width
-// This setup keeps all divisions proportional to each other at all times
-// If you break a division and the resulting divisions are smaller than smallest,
-// the division you're breaking becomes its own TabBar, with expandAll turned on. You can toggle it
-// ^ but this doesn't make sense... if I have [1,1,1,1] and they all have plenty of width, I don't need a toggleable division
-// but how would we know if a division is too small? we'd need to know the width of the division is 
-// we could try container queries...
-
-divnum.value[0].notes[1].note = 70;
-
-function buttonClick () {
-  divnum.value[0].notes[2].note = 60;
-}
-
-const another = ref([1, 1, 1, 1].map(d => createEmptyDivision(d, 6)));
-
+const divnum = ref([createDivision(1, 1, [{note: 66}, { note: 60 }, {note: 60}], [,,,{ note: 55}]), createDivision(3,1, [,,{note: 56}])]);
 
 // const showDivisions = ref(false);
 </script>
 <template>
   <!-- <input type="checkbox" v-model="showDivisions"/> -->
-  <button @click="buttonClick">hehe</button>
   <input type="number" min="2" max="4"/>
   <TabsTabBar v-model="divnum"></TabsTabBar>
-  <TabsTabBar v-model="another"></TabsTabBar>
+  <!-- <TabsTabBar v-model="another"></TabsTabBar> -->
   <Fretboard width="50%"/>
 </template>
 <style scoped>
-div {
-  width: 100%;
-}
+/* div {
+  width: 50%;
+} */
 </style>./components/tabs/data
--- components/tabs/TabBar.vue
+++ components/tabs/TabBar.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { createEmptyDivision, type Division, type NoteData } from './data';
+import { type Division, type NoteData, type NoteStack, timeUnits } from './data';
 import NoteInput from "./NoteInput.vue";
 import type { PropType } from 'vue';
 
@@ -10,16 +10,78 @@ const props = defineProps(
     default: () => defaultTuning,
     validator: (tuning: Midi[], props) => {
       const divisions = props.modelValue as Division[];
-      return divisions.every(d => d.duration > 0 && d.notes.length === tuning.length);
+      const stringsLength = divisions.every(d => d.stacks.every(stack => stack.length <= tuning.length));
+      return stringsLength;
     }
   }, 
   frets: {
     type: Number,
     default: 12,
   },
+  notches: {
+    type: Number,
+    default: 16 
+  }
+
 });
 
-const divisions = defineModel<Division[]>({required: true});
+const divisions = defineModel<Division[]>({
+  required: true,
+  validator: (divisions: Division[]) => {
+    const sorted = divisions.sort((a, b) => a.start - b.start);
+    const validStart = sorted[0].start >= 0;
+    const hasStacks = divisions.every(d => d.stacks.length > 0);
+    const positiveDuration = divisions.every(d => d.duration > 0);
+    const noOverlap = sorted.every((d, i) => {
+      if (i === 0) return true;
+      const prev = sorted[i - 1];
+      return d.start >= prev.start + prev.duration
+    });
+    return validStart && hasStacks && positiveDuration && noOverlap;
+  }
+});
+
+// sort the divisions by start
+const sortedDivisions = computed(() => divisions.value.sort((a, b) => a.start - b.start));
+
+type DivisionRender = {
+  colStart: number,
+  span: number,
+  stacks: NoteStack[],
+}
+
+const divisionRenders = computed(() => {
+  const renderData = divisions.value.map((division, i) => {
+    // 4 notches means each notch is 1 quarter note, 8 notches means each not is 1/2 a quarter note..
+    const unit = 4 / props.notches; // e.g., 4 / 16 = 1/4
+    // TODO: probably change to ceil/floor rather than floor/ceil so we never overlap
+    const colStart = Math.floor(division.start / unit); // e.g. .125 / 0.25 = 0.5 -> 0
+    const span = Math.ceil(division.duration / unit); // e.g. 3.0625 / 0.25 = 12.25 -> 13
+    const remainder = Math.min(division.start % unit, division.duration % unit)
+    const subunit = division.duration / division.stacks.length;
+    // const subunit = timeUnits.find(unit => remainder % unit == 0);
+    if (!subunit) throw new Error("note spacing is off");
+    const startPadding = ((division.start % unit) / subunit); 
+
+    const substacks: NoteStack[] = new Array(startPadding).fill([]);
+    for (let i = 0; i < division.stacks.length; i++) {
+      const stack = division.stacks[i];
+      substacks.push(stack);
+      substacks.push(...new Array(subunit / unit - 1).fill([]));
+    }
+
+    return {
+      colStart,
+      span,
+      stacks: substacks,
+    }
+    // do we need end padding?
+    // generate the stacks. just put a given note in one stack (assume a note lasts for subunit)
+  });
+  // TODO: combine contiguous divisions that have the same number of (stacks / span)
+
+  return renderData;
+});
 
 const strings = computed(() => props.tuning.length);
 
@@ -29,26 +91,24 @@ function modifyDivisions( transform: (division: Division[]) => Division[]) {
   divisions.value = transformed;
 }
 
-function updateNoteData(col: number, string: number, noteData: NoteData) {
-  modifyDivisions((divisions) => {
-    divisions[col].notes[string] = noteData;
-    return divisions;
-  });
-}
+// function updateNoteData(col: number, string: number, noteData: NoteData) {
+//   modifyDivisions((divisions) => {
+//     divisions[col].notes[string] = noteData;
+//     return divisions;
+//   });
+// }
 
-function split(index: number) {
-  modifyDivisions((divisions) => {
-    const curr = divisions[index];
-    const halfTime = curr.duration / 2;
-    divisions.splice(index, 1, {duration: halfTime, notes: curr.notes}, createEmptyDivision(halfTime, strings.value));
-    return divisions;
-  });
-}
+// function split(index: number) {
+//   modifyDivisions((divisions) => {
+//     const curr = divisions[index];
+//     const halfTime = curr.duration / 2;
+//     divisions.splice(index, 1, {duration: halfTime, notes: curr.notes}, createEmptyDivision(halfTime, strings.value));
+//     return divisions;
+//   });
+// }
 
-const lengths = computed(() => divisions.value.map(d => d.duration));
-const smallest = computed(() => Math.min(...lengths.value));
-
-const templateColumns = computed<string>(() => lengths.value.map(length => length + "fr").join(" "));
+// const lengths = computed(() => divisions.value.map(d => d.duration));
+// const smallest = computed(() => Math.min(...lengths.value));
 
 const dragging = ref(0);
 
@@ -56,23 +116,10 @@ const dragging = ref(0);
 
 <template>
   <div class="bar">
-    <div v-for="i in strings" :style="`grid-row: ${i} / span 1`" class="string"></div>
-    <div v-for="({duration, notes}, col) in divisions" class="division"
-      :style="`grid-column: ${col + 1} / span 1`">
-      <div class="notes">
-        <div v-for="(stringTuning, string) in tuning" class="row">
-          <NoteInput
-            :data="notes[string]"
-            :frets="frets"
-            :tuning="stringTuning"
-            @dataChange="updateNoteData(col, string, $event)">
-          </NoteInput>
-        </div>
-      </div>
-      <div v-show="!editing" class="half-bar" @click="split(col)"></div>
-      <div class="divider" @mousedown="dragging = col + 1" @mouseup="dragging = 0"></div>
-    </div>
-    <div v-if="dragging" @mouseup="dragging = 0" @mouseout="dragging = 0" class="dragger"></div>
+    <div v-for="i in strings" :style="`grid-row: ${i} / span 1`" class="string"/>
+    <TabsDivision v-for="{colStart, span, stacks} in divisionRenders"
+      :stacks :tuning :frets
+      :style="`grid-column: ${colStart} / span ${span}`" />
   </div>
 </template>
 
@@ -87,62 +134,10 @@ const dragging = ref(0);
 .bar {
   --min-division-width: 32px;
   display: grid;
-  grid-template-columns: v-bind(templateColumns);
+  grid-template-columns: repeat(v-bind(notches), 1fr);
   /* align-items: center; */
   /* grid-auto-flow: column; */
 }
 
-.subgrid {
-  display: grid;
-  grid: subgrid / subgrid;
-}
-
-.dragger {
-  background-color: red;
-  opacity: 0.3;
-  margin-left: -5px;
-  grid-column: v-bind(dragging) / span 2;
-  grid-row: 1 / span v-bind(strings);
-}
-
-.division {
-  display: flex;
-  justify-content: flex-end;
-  grid-row: 1 / span v-bind(strings);
-}
-
-.divider {
-  height: 100%;
-  width: 2px;
-  background-color: lightgray;
-  border: 2px solid white;
-
-  &:hover {
-    background-color: lightcoral;
-    border-color: lightcoral;
-  }
-}
-
-.notes {
-  margin-right: auto;
-}
-
-.row {
-  display: flex;
-  align-items: center;
-}
-
-.half-bar {
-  height: 100%;
-  min-width: var(--min-division-width);
-  max-width: calc(50%);
-  flex-grow: 1;
-  border-left: 1px dashed transparent;
-}
-
-.half-bar:hover {
-  background-color: rgb(210, 237, 246);
-  border-left: 1px dashed lightgray;
-}
 
 </style>
