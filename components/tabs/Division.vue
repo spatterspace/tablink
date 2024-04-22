4c02f523955aca673f4a5b65a6cc43a7feaa47a7
spatterspace <me@spatter.space>
2024-04-22 11:15:25 -0400

    lil squishies

--- components/tabs/Division.vue
+++ components/tabs/Division.vue
@@ -21,44 +21,35 @@ const emit = defineEmits<{
 const sortedSubstacks = computed(() => props.data.substacks?.toSorted((a, b) => a.notchPosition - b.notchPosition) || []);
 const relativePositions = computed(() => sortedSubstacks.value.map(substack => substack.notchPosition - props.data.notchPosition));
 const subunit = computed(() => SpacingsDescending.find(spacing => relativePositions.value?.every(relative => relative % spacing === 0)) || 1);
-const columns = computed(() => (1 - subunit.value) / subunit.value);
-console.log(columns.value);
+const numColumns = computed(() => (1 - subunit.value) / subunit.value);
+console.log(numColumns.value);
 const colPositions = relativePositions.value.map(pos => pos / subunit.value);
 
 const debugColor = computed(() => `rgb(${props.data.notchPosition % 2 * 255} 150 ${(props.data.notchPosition + 1) % 2 * 255})`);
 </script>
 
 <template>
-  <div class="division"
-       @click="console.log(data)">
+  <div class="division" @click="console.log(data)">
     <div class="stack">
-      <TabsNoteInput v-for="(noteSpot) in props.data.stack"
-                     :key="noteSpot.string"
-                     :data="noteSpot.data"
-                     :tuning="props.tuning[noteSpot.string]"
-                     :frets="props.frets"
-                     @data-change="emit('noteChange', { ...noteSpot, data: $event })"
-      />
+      <TabsNoteInput v-for="(noteSpot) in props.data.stack" :key="noteSpot.string" :data="noteSpot.data"
+        :tuning="props.tuning[noteSpot.string]" :frets="props.frets"
+        @data-change="emit('noteChange', { ...noteSpot, data: $event })" />
     </div>
-    <Expander v-if="sortedSubstacks.length"
-              v-slot="{ expanded }">
-      <div class="substack-grid"
-           :class="{ squish: !expanded }">
+    <Expander v-if="sortedSubstacks.length" v-slot="{ expanded }">
+      <div class="substack-grid" :class="{ squish: !expanded }">
         <TabsStrings />
-        <div v-for="(substack, i) in sortedSubstacks"
-             :key="substack.notchPosition"
-             class="substack"
-             :style="{ gridColumn: colPositions[i] }">
+        <div v-for="(substack, i) in sortedSubstacks" :key="substack.notchPosition" class="substack"
+          :style="{ gridColumn: colPositions[i] }">
           <template v-if="expanded">
-            <TabsNoteInput
-              v-for="(noteSpot) in substack.stack"
-              :key="noteSpot.string"
-              :data="noteSpot.data"
-              :background-color="expanded ? 'white' : 'transparent'"
-              :tuning="props.tuning[noteSpot.string]"
-              :frets="props.frets"
-              @data-change="emit('noteChange', { ...noteSpot, data: $event })"
-            />
+            <TabsNoteInput v-for="noteSpot in substack.stack" :key="noteSpot.string" :data="noteSpot.data"
+              :background-color="expanded ? 'white' : 'transparent'" :tuning="props.tuning[noteSpot.string]"
+              :frets="props.frets" @data-change="emit('noteChange', { ...noteSpot, data: $event })" />
+          </template>
+          <template v-else>
+            <div class="indicator" v-for="notespot in substack.stack">
+              <div v-if="notespot.data" class="square"
+                :style="{ backgroundColor: defaultColors[getChroma(notespot.data.midi)] }" />
+            </div>
           </template>
         </div>
       </div>
@@ -70,7 +61,7 @@ const debugColor = computed(() => `rgb(${props.data.notchPosition % 2 * 255} 150
 .division {
   /* min-width: calc(var(--min-division-width) * 0.75); */
   /* For scrollbar height */
-  height: calc(var(--min-division-width) / 2 * v-bind(numStrings) + 20px);
+  /* height: calc(var(--min-division-width) / 2 * v-bind(numStrings) + 20px); */
   grid-row: 1 / span v-bind(numStrings);
   grid-column: v-bind(column) / span 1;
   /* border-top: 2px solid v-bind(debugColor); */
@@ -89,15 +80,36 @@ const debugColor = computed(() => `rgb(${props.data.notchPosition % 2 * 255} 150
 }
 
 .substack {
-  grid-row: 1 / 1;
+  grid-row: 1 / -1;
+}
+
+.indicator {
+  display: flex;
+  height: calc(var(--min-division-width) / 2);
+  /* border: 1px solid red; */
+  justify-content: flex-start;
+  align-items: center;
+  container-type: size;
+}
+
+.square {
+  width: 60%;
+  max-width: calc(var(--min-division-width) / 4);
+  aspect-ratio: 1;
+  background-color: blue;
+}
+
+@container (aspect-ratio < 0.3) {
+  .square {
+    display: none;
+  }
 }
 
 .substack-grid {
   display: grid;
-  height: calc(var(--min-division-width) / 2 * v-bind(numStrings));
-  grid-template-columns: repeat(v-bind(columns), calc(var(--min-division-width) / 2 / v-bind(columns))) 1fr;
-  /* grid-template-columns: repeat(v-bind(columns), calc(var(--min-division-width) / 2)); */
-  grid-template-rows: 1fr;
-  color: red;
+  /* height: calc(var(--min-division-width) / 2 * v-bind(numStrings)); */
+  /* grid-template-columns: repeat(v-bind(numColumns), calc(var(--min-division-width) / 2 / v-bind(numColumns))) 1fr; */
+  grid-template-columns: repeat(v-bind(numColumns), 1fr);
+  grid-template-rows: repeat(v-bind(numStrings), calc(var(--min-division-width) / 2));
 }
 </style>
--- components/tabs/TabBar.vue
+++ components/tabs/TabBar.vue
@@ -1,22 +1,21 @@
 <script lang="ts" setup>
-import type { NoteData, NoteSpot, FilledSpot } from './data';
-import { SpacingsDescending, Spacing } from './data';
-import type { PropType } from 'vue';
-
+import type { PropType } from "vue";
+import type { NoteData, NoteSpot, FilledSpot } from "./data";
+import { SpacingsDescending, Spacing } from "./data";
 
 type StackData = NoteSpot[];
 
 // don't make this type recursive; the divisions should control their own notches/units
 export type DivisionData = {
-  notchPosition: number,
-  stack: StackData,
+  notchPosition: number
+  stack: StackData
   substacks?: Array<{ notchPosition: number, stack: NoteSpot[] }> // relative to root's notches
-} 
+};
 
 export type SpacerData = {
-  notchStart: number,
-  notches: number;
-}
+  notchStart: number
+  notches: number
+};
 
 const props = defineProps(
   {
@@ -26,7 +25,7 @@ const props = defineProps(
       validator: (tuning: Midi[], props) => {
         const notes = props.modelValue as FilledSpot[];
         return notes.every(note => note.string >= 0 && note.string < tuning.length);
-      }
+      },
     },
     frets: {
       type: Number,
@@ -46,7 +45,7 @@ const props = defineProps(
       validator: (notches: number, props) => {
         const beats = props.beats as number;
         return beats / notches in Spacing;
-      }
+      },
     },
   });
 
@@ -56,11 +55,11 @@ const strings = computed(() => props.tuning.length);
 const notes = defineModel<FilledSpot[]>({
   required: true,
   validator: (notes: FilledSpot[]) => {
-    const noConflict = notes.length === (new Set(notes.map(note => `${note.string},${note.position}`))).size
+    const noConflict = notes.length === (new Set(notes.map(note => `${note.string},${note.position}`))).size;
     const inGrid = notes.every(note => note.position % SpacingsDescending.at(-1)! == 0);
     // TODO
     return noConflict && inGrid;
-  }
+  },
 });
 
 const stackMap = computed(() => {
@@ -74,46 +73,45 @@ const stackMap = computed(() => {
     stackMap.get(note.position)![note.string] = Object.assign({}, note);
   }
   return stackMap;
-})
+});
 
 type StackMap = typeof stackMap.value;
 
 const divisions = computed<DivisionData[]>(() => {
   const sorted = [...stackMap.value].sort(([pos1], [pos2]) => pos1 - pos2);
-  const normalized: Array<[number, NoteSpot[]]> = sorted.map( 
+  const normalized: Array<[number, NoteSpot[]]> = sorted.map(
     ([pos, stack]) => ([pos / unit.value, stack]
-  ));
+    ));
 
   return normalized.reduce<DivisionData[]>(
     (acc, [notchPosition, stack]) => {
       if (Number.isInteger(notchPosition)) {
         acc.push({
           notchPosition,
-          stack
-        })
+          stack,
+        });
         return acc;
       }
       const prev = acc.at(-1);
       const hasParent = prev?.notchPosition === Math.floor(notchPosition);
       if (hasParent) {
         const substacks = prev.substacks ?? [];
-        substacks.push({ notchPosition, stack})
+        substacks.push({ notchPosition, stack });
         prev.substacks = substacks;
         return acc;
       }
       acc.push({
         notchPosition: Math.floor(notchPosition),
         stack: [],
-        substacks: [{ notchPosition, stack}]
-      })
+        substacks: [{ notchPosition, stack }],
+      });
       return acc;
-    }, 
-  [])
-
-})
+    },
+    []);
+});
 
 /*
-  e.g: 0, 8, 9, 12, 15 
+  e.g: 0, 8, 9, 12, 15
   gaps: 8, 1, 3, 3
   slots: 7, 0, 2, 2
   (position + 1) span (slots): (0 + 1) span 7, (8 + 1) span 0, (9 + 1) span 2, (12 + 1) span 2
@@ -128,14 +126,13 @@ const emptyDivisions = computed<SpacerData[]>(() => {
     if (slot) {
       spacers.push({ notchStart: filledPositions[i] + 1, notches: slot });
     }
-  })
+  });
   return spacers;
-})
-
+});
 
 function modifyNotes(transform: (map: StackMap) => StackMap) {
   const transformed = transform(stackMap.value);
-  const stacks = transformed.values(); 
+  const stacks = transformed.values();
   const newNotes: FilledSpot[] = [];
   for (const note of Array.from(stacks).flat()) {
     if (note.data) {
@@ -146,23 +143,23 @@ function modifyNotes(transform: (map: StackMap) => StackMap) {
 }
 
 function updateNote(note: NoteSpot) {
-  modifyNotes(stackMap => {
+  modifyNotes((stackMap) => {
     if (stackMap.has(note.position)) {
       stackMap.get(note.position)![note.string] = note;
-    } else {
+    }
+    else {
       stackMap.set(note.position, [note]);
     }
     return stackMap;
-  })
+  });
 }
 
 const newNoteCol = ref(0);
 const newNoteRow = ref(0);
 const showNewNote = ref(false);
-const newNoteData = ref<NoteData>()
+const newNoteData = ref<NoteData>();
 
-
-function emptySpotHover (string: number, notch: number) {
+function emptySpotHover(string: number, notch: number) {
   showNewNote.value = true;
   newNoteCol.value = notch;
   newNoteRow.value = string;
@@ -175,23 +172,28 @@ function emptySpotHover (string: number, notch: number) {
     <TabsDivision
       v-for="data in divisions"
       :key="data.notchPosition"
-      :data :tuning :frets
-      @note-change="updateNote" />
+      :data
+      :tuning
+      :frets
+      @note-change="updateNote"
+    />
     <TabsSpacer
       v-for="data in emptyDivisions"
       :key="data.notchStart"
-      :data="data"  :strings="strings" 
+      :data="data"
+      :strings="strings"
       @hover="emptySpotHover"
-      />
-    <TabsNoteInput 
+    />
+    <TabsNoteInput
       v-if="showNewNote"
-      start-focused 
+      start-focused
       collapse
       class="new-note"
-      :data="newNoteData" 
+      :data="newNoteData"
       :tuning="tuning[newNoteRow - 1]"
-      :frets="frets" 
-      @mouseleave="() => showNewNote = false"/>
+      :frets="frets"
+      @mouseleave="() => showNewNote = false"
+    />
   </div>
 </template>
 
