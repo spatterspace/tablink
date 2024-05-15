857403746a42ffa47fce64d18120b5e9e78ade3f
Vocal Unrest <vocalunrest@icloud.com>
2024-05-15 02:05:36 +0000

    parent Tab component

--- components/tab/bar/TabBar.vue
+++ components/tab/bar/TabBar.vue
@@ -12,49 +12,48 @@ export type DivisionData = {
   substacks?: Array<{ notchPosition: number, stack: NoteSpot[] }> // relative to root's notches
 };
 
-const props = defineProps(
-  {
-    tuning: {
-      type: Array as PropType<Midi[]>,
-      default: () => defaultTuning,
-      validator: (tuning: Midi[], props) => {
-        const notes = props.modelValue as FilledSpot[];
-        return notes.every(note => note.string >= 0 && note.string < tuning.length);
-      },
+const props = defineProps({
+
+  tuning: {
+    type: Array as PropType<Midi[]>,
+    default: () => defaultTuning,
+  },
+
+  frets: {
+    type: Number,
+    default: 24,
+  },
+
+  beats: {
+    type: Number,
+    default: 4 * Spacing.Quarter,
+    validator: (beats: number, props) => {
+      const notes = props.modelValue as FilledSpot[];
+      return notes.every(note => note.position >= 0 && note.position < beats);
     },
-    frets: {
-      type: Number,
-      default: 24,
+  },
+
+  notches: {
+    type: Number,
+    default: 16,
+    validator: (notches: number, props) => {
+      const beats = props.beats as number;
+      return beats / notches in Spacing;
     },
-    beats: {
-      type: Number,
-      default: Spacing.Quarter * 4,
-      validator: (beats: number, props) => {
-        const notes = props.modelValue as FilledSpot[];
-        return notes.every(note => note.position >= 0 && note.position < beats);
-      },
-    },
-    notches: {
-      type: Number,
-      default: 16,
-      validator: (notches: number, props) => {
-        const beats = props.beats as number;
-        return beats / notches in Spacing;
-      },
-    },
-  });
+  },
+
+  // Where in the noteData does this start?
+  notePosition: {
+    type: Number,
+    default: 0,
+  },
+});
 
 const unit = computed<Spacing>(() => props.beats / props.notches);
 const strings = computed(() => props.tuning.length);
 
 const notes = defineModel<FilledSpot[]>({
   required: true,
-  validator: (notes: FilledSpot[]) => {
-    const noConflict = notes.length === (new Set(notes.map(note => `${note.string},${note.position}`))).size;
-    const inGrid = notes.every(note => note.position % smallestSpacing == 0);
-    // TODO
-    return noConflict && inGrid;
-  },
 });
 
 const stackMap = computed(() => {
@@ -79,7 +78,7 @@ type StackMap = typeof stackMap.value;
 const divisions = computed<DivisionData[]>(() => {
   const sorted = [...stackMap.value].sort(([pos1], [pos2]) => pos1 - pos2);
   const normalized: Array<[number, NoteSpot[]]> = sorted.map(
-    ([pos, stack]) => ([pos / unit.value, stack]
+    ([pos, stack]) => ([(pos - props.notePosition) / unit.value, stack]
     ));
 
   return normalized.reduce<DivisionData[]>(
@@ -108,8 +107,6 @@ const divisions = computed<DivisionData[]>(() => {
     },
     []);
 });
-
-console.log("divisions", divisions.value);
 
 function modifyNotes(transform: (map: StackMap) => StackMap) {
   const transformed = transform(stackMap.value);
