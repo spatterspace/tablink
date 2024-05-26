7aa39b1755074285367f758e852d8e95ac9dc85f
vocalunrest <vocalunrest@icloud.com>
2024-05-25 22:18:26 -0400

    rewrite model

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { NoteSpot } from "./components/tab/data";
+import type { GuitarNote } from "./components/tab/data";
 import { Spacing, createTabStore } from "./components/tab/data";
 import Tab from "./components/tab/Tab.vue";
 import { SelectionStateKey, createSelectionState } from "./components/tab/providers/selection-state";
@@ -7,40 +7,41 @@ import { SelectionStateKey, createSelectionState } from "./components/tab/provid
 const selectionState = createSelectionState();
 provide(SelectionStateKey, selectionState);
 
-const notes = createTabStore();
+const tabStore = createTabStore();
 
-notes.setNote(0, 0, "B4");
-notes.setNote(Spacing.Quarter * 3, 0, "B4");
-notes.setNote(Spacing.Quarter * 4 - Spacing.Sixteenth, 0, "B4");
-/* notes.setNote(Spacing.Quarter * 4, 0, "F4");
-notes.setNote(Spacing.Quarter * 5, 1, "E4");
-// notes.setNote(Spacing.Quarter * 5 + Spacing.ThirtySecond * 3, 0, "E4");
-notes.setNote(Spacing.Quarter * 6, 2, "G4");
-notes.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth, 3, "G4");
-notes.setNote(Spacing.Quarter * 7 + Spacing.ThirtySecond, 4, "A4"); */
-notes.setNote(Spacing.Quarter * 8, 5, "F3");
-notes.setNote(Spacing.Quarter * 9, 0, "F4");
-notes.setNote(Spacing.Quarter * 9 + Spacing.ThirtySecond, 0, "F4");
-notes.setNote(Spacing.Quarter * 8, 2, "A4"),
+const guitar = tabStore.createGuitarTab();
+guitar.setNote(0, 0, "B4");
+guitar.setNote(Spacing.Quarter * 3, 0, "B4");
+guitar.setNote(Spacing.Quarter * 4 - Spacing.Sixteenth, 0, "B4");
+/* guitar.setNote(Spacing.Quarter * 4, 0, "F4");
+guitar.setNote(Spacing.Quarter * 5, 1, "E4");
+// guitar.setNote(Spacing.Quarter * 5 + Spacing.ThirtySecond * 3, 0, "E4");
+guitar.setNote(Spacing.Quarter * 6, 2, "G4");
+guitar.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth, 3, "G4");
+guitar.setNote(Spacing.Quarter * 7 + Spacing.ThirtySecond, 4, "A4"); */
+guitar.setNote(Spacing.Quarter * 8, 5, "F3");
+guitar.setNote(Spacing.Quarter * 9, 0, "F4");
+guitar.setNote(Spacing.Quarter * 9 + Spacing.ThirtySecond, 0, "F4");
+guitar.setNote(Spacing.Quarter * 8, 2, "A4"),
 
-notes.setNote(Spacing.Quarter * 2, 0, "C5");
+guitar.setNote(Spacing.Quarter * 2, 0, "C5");
 
-notes.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth, 1, "G5");
-notes.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 2, 4, "B3");
-notes.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 3, 4, "C4");
-notes.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 3, 5, "F3");
+guitar.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth, 1, "G5");
+guitar.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 2, 4, "B3");
+guitar.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 3, 4, "C4");
+guitar.setNote(Spacing.Quarter * 2 + Spacing.SixtyFourth * 3, 5, "F3");
 
-notes.setNote(Spacing.Quarter * 2 + Spacing.Sixteenth, 5, "F2");
+guitar.setNote(Spacing.Quarter * 2 + Spacing.Sixteenth, 5, "F2");
 
-notes.setNote(Spacing.Quarter * 6, 0, "C5");
+guitar.setNote(Spacing.Quarter * 6, 0, "C5");
 
-notes.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth, 1, "G5");
-// notes.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 2, 4, "B3");
-notes.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 3, 4, "C4");
-// notes.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 3, 5, "F3");
+guitar.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth, 1, "G5");
+// guitar.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 2, 4, "B3");
+guitar.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 3, 4, "C4");
+// guitar.setNote(Spacing.Quarter * 6 + Spacing.SixtyFourth * 3, 5, "F3");
 
-notes.setNote(Spacing.Quarter * 7 + Spacing.SixtyFourth * 2, 1, "G5");
-notes.setNote(Spacing.Quarter * 7 + Spacing.SixtyFourth * 3, 4, "B3");
+guitar.setNote(Spacing.Quarter * 7 + Spacing.SixtyFourth * 2, 1, "G5");
+guitar.setNote(Spacing.Quarter * 7 + Spacing.SixtyFourth * 3, 4, "B3");
 
 const activeStack = computed(() => {
   if (selectionState.start) {
@@ -49,7 +50,7 @@ const activeStack = computed(() => {
   return notes.tuning.map((_, string) => ({ position: -1, string }));
 });
 
-function fretboardNoteChange(note: NoteSpot) {
+function fretboardNoteChange(note: GuitarNote) {
   const { position, string, data } = note;
   if (data) {
     notes.setNote(position, string, data);
