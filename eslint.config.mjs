ba3a6e3c71ce3ce106860b389e9cb57bf0a5bbd7
spatterspace <me@spatter.space>
2024-06-05 19:35:48 -0400

    clean eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
+export default withNuxt({
+  files: ["**/*.vue"],
+  rules: {
+    "vue/require-v-for-key": "off",
+    "vue/html-self-closing": ["error", {
+      "html": {
+        "void": "always", // match prettier
+        "normal": "always",
+        "component": "always"
+      },
+      "svg": "always",
+      "math": "always"
+    }]
+  },
+},
 )
