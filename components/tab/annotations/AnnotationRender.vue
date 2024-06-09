f0158e0edc56af07df0a0cbf29b2d06e0c3fa514
vocalunrest <vocalunrest@icloud.com>
2024-06-09 16:45:29 -0400

    edit and delete annotations

--- components/tab/annotations/AnnotationRender.vue
+++ components/tab/annotations/AnnotationRender.vue
@@ -8,22 +8,74 @@ const props = defineProps<{
   annotation?: Annotation;
 }>();
 
+const emit = defineEmits<{
+  updateTitle: [string];
+  delete: [];
+}>();
+
 const pointerEvents = computed(() => (props.annotation ? "auto" : "none"));
+
+const titleEl = ref<HTMLDivElement>();
+
+function titleInput() {
+  if (props.annotation) {
+    const value = titleEl.value!.innerText;
+    emit("updateTitle", value);
+  }
+}
+
+function titleFocus() {
+  window.getSelection()?.selectAllChildren(titleEl.value!);
+  titleEl.value!.scrollTo({ left: 0 });
+}
+
+watch(
+  () => props.annotation,
+  (data) => {
+    if (data) {
+      console.log("here");
+      setTimeout(() => titleEl.value!.focus(), 1);
+    }
+  },
+);
 </script>
 
 <template>
   <div :class="`annotation annotation-${row}`">
-    {{ annotation?.title }}
+    <div ref="titleEl" class="title" contenteditable @input="titleInput" @focus="titleFocus">
+      {{ annotation?.title }}
+    </div>
+    <div v-if="annotation" class="delete" @click="emit('delete')">&Cross;</div>
   </div>
 </template>
 
 <style scoped>
 .annotation {
-  border: 1px solid gray;
+  display: flex;
+  align-items: center;
+  border-right: 1px solid gray;
   grid-column-start: v-bind(startColumn);
   grid-column-end: v-bind(endColumn);
   grid-row: v-bind(row);
   background-color: lightblue;
   pointer-events: v-bind(pointerEvents);
+}
+
+.title {
+  overflow: hidden;
+  white-space: nowrap;
+  /* text-overflow: ellipsis; */
+  flex-grow: 1;
+  text-align: center;
+}
+
+.delete {
+  color: maroon;
+  cursor: pointer;
+  padding: 0px 1px;
+  &:hover {
+    font-weight: bold;
+    color: darkred;
+  }
 }
 </style>
